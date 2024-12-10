ddfc2b4632be2da3d312b22e95c768cae19c0faf
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-12-10 16:25:26 +0545

    refactor(auth): update authOptions import and remove unused auth.ts file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth"; 
+import { authOptions } from '@/auth'
 
 console.log('authOptions:', authOptions);
 const handler = NextAuth(authOptions);
