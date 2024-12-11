8c9d72cb78319dd3a5407a95b3fbebd601c34a31
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-12-11 09:56:25 +0545

    refactor(layout): update component imports and improve styling; adjust Tailwind CSS configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-
+import './globals.css'
 export default function RootLayout({
   children,
 }: {
@@ -7,7 +7,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-          {children}
+        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      Home 
+    <div className='text-3xl'>
+      Home
     </div>
   );
 }
